c949a0a8f3d6674113ffd1f66398a66da4196697
Sitthata <aprils96950@gmail.com>
2024-08-22 20:55:20 +0700

    feat: Add projectsData to projects component

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -2,23 +2,7 @@ import VideoPlayer from './video-player'
 import ProjectCard from './project-card'
 import { Button } from '../ui/button'
 import { FaArrowRight } from 'react-icons/fa'
-
-const projectsData = [
-    {
-        title: 'Kanban Board Fullstack',
-        imgSrc: 'bg-kanban-bg',
-        badges: ['Vue.js', 'Spring Boot', 'MySQL'],
-        description:
-            'A full-stack project with JWT Authentication that allows users to create, update, and delete tasks. The frontend is built with Vue.js and the backend is built with Spring Boot.',
-    },
-    {
-        title: 'Dictionary App',
-        imgSrc: 'bg-dictionary-app',
-        badges: ['React', 'Tailwind', 'Dictionary API'],
-        description:
-            'A simple dictionary app that allows users to search for the meaning of words. The app is built with React and Tailwind CSS.',
-    },
-]
+import { projectsData } from './data'
 
 export default function Projects() {
     return (
