383e40e5f999fdbd75a07da2ea6e38e9b9cfafbc
Sitthata <aprils96950@gmail.com>
2024-08-22 20:51:17 +0700

    feat: Add new icons and tooltips to skills section

--- src/components/skills-section/data.tsx
+++ src/components/skills-section/data.tsx
@@ -1,32 +1,60 @@
 import { RiNextjsLine, RiTailwindCssFill } from 'react-icons/ri'
 import { SiExpress, SiSpringboot } from 'react-icons/si'
 import { BsFiletypeSql } from 'react-icons/bs'
-import { FaReact } from 'react-icons/fa'
+import { FaDocker, FaGithub, FaLinux, FaReact, FaVuejs } from 'react-icons/fa'
 import { CardData } from '@/types/type'
 
 export const cardData: CardData[] = [
     {
         title: 'React.js',
         icon: <FaReact />,
+        tipContent: 'Frontend library for building user interfaces',
+    },
+    {
+        title: 'Vue.js',
+        icon: <FaVuejs />,
+        tipContent: 'Frontend framework for building user interfaces',
     },
     {
         title: 'Next.js',
         icon: <RiNextjsLine />,
+        tipContent: 'SSA React framework for production',
     },
     {
         title: 'Spring Boot',
         icon: <SiSpringboot />,
+        tipContent: 'Java-based framework for building Backend applications',
     },
     {
         title: 'Express.js',
         icon: <SiExpress />,
+        tipContent: 'Node.js framework for building Backend applications',
     },
     {
         title: 'SQL',
         icon: <BsFiletypeSql />,
+        tipContent: 'Structured Query Language for managing databases',
     },
     {
         title: 'Tailwind',
         icon: <RiTailwindCssFill />,
+        tipContent: 'Utility-first CSS framework',
+    },
+    {
+        title: 'Git/Github',
+        icon: <FaGithub />,
+        tipContent:
+            'Version control system for tracking changes in source code',
+    },
+    {
+        title: 'Docker',
+        icon: <FaDocker />,
+        tipContent:
+            'Containerization platform for building, shipping, and running applications',
+    },
+    {
+        title: 'Linux OS',
+        icon: <FaLinux />,
+        tipContent: 'Open-source operating system',
     },
 ]
