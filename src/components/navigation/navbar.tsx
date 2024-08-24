0bf86b252b47e831dc3bc2a1d2ed6768221948b3
Sitthata <aprils96950@gmail.com>
2024-08-24 23:33:08 +0700

    refactor: Update navbar component with framer-motion and lazy loading for images

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,32 +1,42 @@
 'use client'
-import { useScroll, motion, useTransform } from 'framer-motion'
+import { useScroll, motion, useMotionValueEvent } from 'framer-motion'
 import NavList from './nav-list'
+import { useState } from 'react'
 
 export default function Navbar() {
     const { scrollY } = useScroll()
+    const [isScrolled, setIsScrolled] = useState(false)
 
-    // Use transform to determine whether the navbar should be in its scrolled state
-    const opacity = useTransform(
-        scrollY,
-        [0, 100],
-        [1, 0.9],
-    )
-    const blur = useTransform(scrollY, [0, 50], ['blur(0px)', 'blur(5px)'])
-    const borderBottom = useTransform(
-        scrollY,
-        [0, 50],
-        ['none', '1px solid rgba(200, 200, 200, 0.1)'],
-    )
+    useMotionValueEvent(scrollY, 'change', (latest: number) => {
+        console.log(latest)
+        if (latest >= 110) {
+            setIsScrolled(true)
+            console.log('Scrolled')
+        } else {
+            setIsScrolled(false)
+            console.log('Not Scrolled')
+        }
+    })
+
+    const navVariant = {
+        initial: {
+            height: '120px',
+            backgroundColor: 'rgba(0,0,0,0)',
+            borderBottom: '1px solid rgba(255,255,255,0.0)',
+        },
+        scrolled: {
+            height: '80px',
+            backgroundColor: 'rgba(0,0,0,0.8)',
+            borderBottom: '1px solid rgba(255,255,255,0.1)',
+        },
+    }
 
     return (
         <motion.nav
-            className="sticky top-0 p-5 z-50 bg-background"
-            style={{
-                opacity: opacity,
-                backdropFilter: blur,
-                borderBottom: borderBottom,
-            }}
-            transition={{ type: 'tween', duration: 0.3 }}
+            className="sticky top-0 p-5 z-50"
+            variants={navVariant}
+            animate={isScrolled ? 'scrolled' : 'initial'}
+            transition={{ type: 'tween', duration: 0.2 }}
         >
             <div className="m-auto max-w-[1150px] flex justify-between">
                 <h1 className="text-3xl font-bold font-jetbrain hover:text-secondary cursor-pointer duration-150">
